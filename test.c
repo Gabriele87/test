d1a0e966606fc4701538f38529d5fcda971fa957
Gabriele <gabriele.sereni@indiotech.com>
2020-03-11 17:55:08 +0100

    Initial commit

---
+++ test.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+typedef struct prova
+{
+    int a,
+    int c, 
+    float d
+    /* data */
+}prova_t;
+
+prova_t prova1={10,12,1.1};
+
+ int main(void){
+     
+     return 0;
+ }
