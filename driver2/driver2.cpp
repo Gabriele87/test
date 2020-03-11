5a0d5c1456fd84bd006491bd51979897bf833734
Gabriele <gabriele.sereni@indiotech.com>
2020-03-11 19:24:36 +0100

    aggiunto driver 2:

---
+++ driver2/driver2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std
+class Animal{
+public:
+	str getFurColor(void);
+	str getWeight(void);
+
+private:
+int weight;
+str fur_color;
+};
+
+str Animal::getFurColor(void){ 
+return fur_color;
+}
+
+class Dog:
+
+
+int main(void){
+return 0;
+}
